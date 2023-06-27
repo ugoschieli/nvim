5ac1b45cf233b5d696d07fe1860793813c8508fa
Ugo Schieli <ugo.schieli@gmail.com>
2023-06-27 21:39:22 +0200

    Add on_attach with LSP keybindings

--- init.lua
+++ init.lua
@@ -89,13 +89,35 @@ local plugins = {
       {
         'folke/neodev.nvim',
         opts = {},
-      }
+      },
     },
     opts = {},
     config = function(_, opts)
       local lspconfig = require 'lspconfig'
 
+      local on_attach = function(client, bufnr)
+        local bufopts = { buffer = bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<space>f', function()
+          vim.lsp.buf.format { async = true }
+        end, bufopts)
+      end
+
       lspconfig.lua_ls.setup {
+        on_attach = on_attach,
         settings = {
           Lua = {
             workspace = {
@@ -105,7 +127,9 @@ local plugins = {
         },
       }
 
-      lspconfig.hls.setup {}
+      lspconfig.hls.setup {
+        on_attach = on_attach,
+      }
     end,
   },
   {
@@ -140,7 +164,14 @@ local plugins = {
 
 require('lazy').setup(plugins, lazy_opts)
 
+-- KEYBINDINGS
+
 vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
 vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
 vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
 vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
