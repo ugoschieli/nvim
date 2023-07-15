7e7932364382b57a58b2a19f23930d9ebc62d27d
Ugo Schieli <ugo.schieli@gmail.com>
2023-07-15 12:14:21 +0200

    Telescope keybindings

--- init.lua
+++ init.lua
@@ -142,7 +142,13 @@ local plugins = {
     opts = {},
     config = function(_, opts)
       require('telescope').setup(opts)
-      require('telescope').load_extension('fzf')
+      require('telescope').load_extension 'fzf'
+
+      local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>ff', builtin.find_files)
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+      vim.keymap.set('n', '<leader>fb', builtin.buffers)
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags)
     end,
   },
   {
