81e4a084f69d946711b9a7943b70e49cbf3e346a
Ugo <ugo.schieli@epitech.eu>
2023-12-24 16:40:02 +0100

    Duck + format on save with conform.nvim

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -20,4 +20,11 @@ return {
     event = 'InsertEnter',
     opts = {},
   },
+  {
+    'tamton-aquib/duck.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>dd', function() require("duck").hatch() end, {})
+      vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
+    end
+  }
 }
