3652c3ed0ae4d4db9a3ce88d47bcc39141b707fd
Ugo <ugo.schieli@epitech.eu>
2023-12-24 16:47:51 +0100

    Lazy loading for conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,18 @@
 return {
   {
     'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = 'Format buffer',
+      },
+    },
     opts = {
       formatters_by_ft = {
         lua = { 'stylua' },
